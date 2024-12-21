18ef27cef332d155e27f971f16dd56285e581242
HeyFang <heyfang36@yahoo.com>
2024-12-21 13:01:52 +0530

    completed math interpreter

---
+++ week 1/interpreter.py
@@ -0,0 +1,20 @@
+exp = input("Expression: ").strip()
+exp1 = exp.split()
+
+x = int(exp1[0])
+y = exp1[1]
+z = int(exp1[2])
+
+match y:
+    case "+":
+        ans = float(x + z)
+    case "-":
+        ans = float(x - z)
+    case "*":
+        ans = float(x * z)
+    case "/":
+        ans = float(x / z)
+    case _:
+        ans = "Invalid operator"
+
+print(f"{ans:.1f}")
